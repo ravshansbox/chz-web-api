80a7b737861b0dd4ee2e4d44f70c6f23a2f7d2fe
Ravshan Samandarov <ravshansbox@gmail.com>
2023-03-13 17:04:39 +0300

    fix getCustomers search params

--- src/routers/customers/getCustomers.ts
+++ src/routers/customers/getCustomers.ts
@@ -1,23 +1,19 @@
 import { sendJson, type Route } from '@ravshansbox/mini-app';
-import { z } from 'zod';
+import { ok } from 'node:assert';
 import { checkPermission } from '../../common/checkPermission';
 import { parseAccessToken } from '../../common/parseAccessToken';
-import { validate } from '../../common/validate';
 import { prismaClient } from '../../prismaClient';
-
-const searchParamsSchema = z.object({
-  company_id: z.string().uuid(),
-});
 
 export const getCustomers: Route = {
   method: 'GET',
   path: '',
-  handler: async ({ searchParams: rawSearchParams, request, response }) => {
-    const searchParams = validate(searchParamsSchema, rawSearchParams);
+  handler: async ({ searchParams, request, response }) => {
+    const companyId = searchParams.get('company_id');
+    ok(companyId, '"company_id" search param is required');
     const accessToken = await parseAccessToken(request);
-    await checkPermission(searchParams.company_id, accessToken.user_id);
+    await checkPermission(companyId, accessToken.user_id);
     const customers = await prismaClient.customer.findMany({
-      where: { company_id: searchParams.company_id },
+      where: { company_id: companyId },
     });
     sendJson(response, customers, 200);
   },
