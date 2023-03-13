e33e906b2822aab81d88db86c585c82e0e17204a
Ravshan Samandarov <ravshansbox@gmail.com>
2023-03-13 13:55:26 +0300

    getOrders: read company_id from search params

--- src/routers/orders/getOrders.ts
+++ src/routers/orders/getOrders.ts
@@ -1,22 +1,18 @@
-import { parseJsonBody, sendJson, type Route } from '@ravshansbox/mini-app';
-import { z } from 'zod';
+import { sendJson, type Route } from '@ravshansbox/mini-app';
+import { ok } from 'assert';
 import { checkPermission } from '../../common/checkPermission';
 import { parseAccessToken } from '../../common/parseAccessToken';
-import { validate } from '../../common/validate';
 import { prismaClient } from '../../prismaClient';
-
-const bodySchema = z.object({
-  company_id: z.string().uuid(),
-});
 
 export const getOrders: Route = {
   method: 'GET',
   path: '',
-  handler: async ({ request, response }) => {
+  handler: async ({ searchParams, request, response }) => {
     const accessToken = await parseAccessToken(request);
-    const body = validate(bodySchema, await parseJsonBody(request));
-    await checkPermission(body.company_id, accessToken.user_id);
-    const orders = await prismaClient.order.findMany({ where: { company_id: body.company_id } });
+    const companyId = searchParams.get('company_id');
+    ok(companyId, '"company_id" search param is required');
+    await checkPermission(companyId, accessToken.user_id);
+    const orders = await prismaClient.order.findMany({ where: { company_id: companyId } });
     sendJson(response, orders, 200);
   },
 };
